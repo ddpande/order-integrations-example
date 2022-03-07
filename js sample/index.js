aca8c7f4e0a4176b0d5fcb9787453b17c57a025b
Pande Darmayasa <pande@maxsol.id>
2022-03-07 15:02:01 +0800

    sample

---
+++ js sample/index.js
@@ -0,0 +1,292 @@
+const axios = require('axios');
+const main_url = "https://beta.rewardplus-staging.com/"
+const token = "156a48c8-eb09-4d06-81ae-d730ab7c7207";
+
+const express = require('express');
+const app = express();
+
+app.get('/', async (req, res) => {
+    res.send(await accountInfo());
+});
+
+app.get('/getProductList', async (req, res) => {
+    res.send(await getProductList('', '', ''));
+});
+app.get('/getVoucherList', async (req, res) => {
+    res.send(await getVoucherList(''));
+});
+app.get('/getBankList', async (req, res) => {
+    res.send(await getBankList());
+});
+app.get('/getOrderDetail', async (req, res) => {
+    res.send(await getOrderDetail(31940));
+});
+app.get('/getOrderList', async (req, res) => {
+    res.send(await getOrderList('', '', '', ''));
+});
+app.get('/getDirectStatus', async (req, res) => {
+    res.send(await getDirectStatus('BULK20220301-6DACLZ'));
+});
+app.get('/generateEvoucherLinks', async (req, res) => {
+    /** add your data
+  * if productCode status isDenom == true && productType == EWALLET, you have to put amount. 
+  * Becouse that product doesn't have fixed price.
+  * if ProductType == BANK, you have to put amount.
+  */
+    const data = {
+        "bankCode": "bni",
+        "productType": "BANK",
+        "amount": 10000,
+        "deliveryMethod": "FILE",
+        "useInventoryService": true,
+        "count": 1
+    };
+    res.send(await generateEvoucherLinks(data));
+});
+app.get('/directSending', async (req, res) => {
+    /** add your data
+     * if productCode status isDenom == true && productType == EWALLET, you have to put amount. 
+     * Becouse that product doesn't have fixed price.
+     * if ProductType == BANK, you have to put amount.
+     */
+    /** Example value $data_customer :
+    * $data_customer = [
+    *  [
+    *      "customerNo" => "081212345678",
+    *      "customerEmail" => "",
+    *      "customerName" => "Test User",
+    *      "productCode" => "OVO20H",
+    *  ],
+    *  [
+    *      "customerNo" => "081199991111",
+    *      "customerEmail" => "",
+    *      "customerName" => "Test User2",
+    *      "productCode" => "OVO10H",
+    *  ],         * 
+    * ]
+    */
+    const data = {
+        "productCode": "SHOP25H",
+        "productType": "EWALLET",
+        "deliveryMethod": "BULK",
+        "useInventoryService": true,
+        "customers": [
+            {
+                "customerNo": "0811999999991111",
+                "customerEmail": "",
+                "customerName": "Test User"
+            }
+        ]
+    };
+    res.send(await directSending(data));
+});
+app.get('/createMultiDenom', async (req, res) => {
+    const data = {
+        "productType": "EWALLET",
+        "deliveryMethod": "FILE",
+        "useInventoryService": true,
+        "customers": [
+            {
+                "customerNo": "081234567890123456",
+                "customerEmail": "someuser@gmail.com",
+                "customerName": "Sample User",
+                "productCode": "OVO20H"
+            }
+        ]
+    }
+    res.send(await createMultiDenom(data));
+});
+
+app.listen(3000, () => {
+    console.log('server start at port 3000');
+});
+
+
+/** Check your account info*/
+async function accountInfo() {
+    const headers = {
+        'integrations-order': token
+    };
+    const api = axios.create({ headers, responseType: 'html' });
+
+    const result = await api.get(`${main_url}order-integration/account`).then(resp => {
+        return resp.data;
+    })
+    return result;
+}
+
+/** check product list , you can filter by exired_date, category or name*/
+async function getProductList(expiry, category, name) {
+    const headers = {
+        'integrations-order': token
+    };
+    const api = axios.create({ headers, responseType: 'json' });
+
+    const result = await api.get(`${main_url}order-integration/products?page=1&itemsPerPage=100&sortBy=createdAt&descending=true&filter[expiry]=${expiry}&filter[category]=${category}&filter[name]=${name}`)
+        .then(resp => {
+            return resp.data;
+        });
+    return result
+}
+
+/** check voucher list you also can filter by name*/
+async function getVoucherList(name) {
+    const headers = {
+        'integrations-order': token
+    };
+    const api = axios.create({ headers, responseType: 'json' });
+
+    const result = await api.get(`${main_url}order-integration/vouchers?filter[name]=${name}&page=1&itemsPerPage=10&sortBy=id&descending=true`)
+        .then(resp => {
+            return resp.data;
+        });
+    return result
+}
+
+/** check list bank exist*/
+async function getBankList() {
+    const headers = {
+        'integrations-order': token
+    };
+    const api = axios.create({ headers, responseType: 'json' });
+
+    const result = await api.get(`${main_url}order-integration/bank-list`)
+        .then(resp => {
+            return resp.data;
+        });
+    return result
+}
+
+/** check your order detail by id*/
+async function getOrderDetail(order_id) {
+    const headers = {
+        'integrations-order': token
+    };
+    const api = axios.create({ headers, responseType: 'json' });
+
+    const result = await api.get(`${main_url}order-integration/orders/${order_id}`)
+        .then(resp => {
+            return resp.data;
+        });
+    return result
+}
+
+/** check order list you can filter by bankcode, price, productCode or transId*/
+async function getOrderList(bankCode, price, productCode, transId) {
+    const headers = {
+        'integrations-order': token
+    };
+    const api = axios.create({ headers, responseType: 'json' });
+    const url = `${main_url}order-integration/orders?page=1&itemsPerPage=10&sortBy=createdAt&descending=false&filter[bankCode]=${bankCode}&filter[price]=${price}&filter[productCode]=${productCode}&filter[transId]=${transId}`
+    const result = await api.get(url)
+        .then(resp => {
+            return resp.data;
+        });
+    return result;
+}
+
+/**check order filter by BulkId (you can get bulk Id when create Order)*/
+async function getDirectStatus(bulkId) {
+    const headers = {
+        'integrations-order': token
+    };
+    const api = axios.create({ headers, responseType: 'json' });
+    const result = await api.get(`${main_url}order-integration/orders?filter[bulkId]=${bulkId}`)
+        .then(resp => {
+            return resp.data;
+        });
+    return result;
+}
+
+/** create evoucher link 
+ * Method FILE for generate Links
+ * example ProductType = BANK
+ */
+/** add your data
+ * if productCode status isDenom == true && productType == EWALLET, you have to put amount. 
+ * Becouse that product doesn't have fixed price.
+ * if ProductType == BANK, you have to put amount.
+ */
+async function generateEvoucherLinks(data) {
+    const headers = {
+        'integrations-order': token
+    };
+
+    const api = axios.create({ headers, responseType: 'json' });
+    const result = await api.post(`${main_url}order-integration/bulk-order`, data)
+        .then(resp => {
+            return resp.data;
+        });
+    return result;
+}
+
+/** create order Bulk example: productType = EWALLET */
+/** add your data
+ * if productCode status isDenom == true && productType == EWALLET, you have to put amount. 
+ * Becouse that product doesn't have fixed price.
+ * if ProductType == BANK, you have to put amount.
+ */
+async function directSending(data) {
+    const headers = {
+        'integrations-order': token
+    };
+
+
+    const api = axios.create({ headers, responseType: 'json' });
+    const result = await api.post(`${main_url}order-integration/bulk`, data)
+        .then(resp => {
+            return resp.data;
+        });
+    return result;
+}
+
+
+/** create order multi-denom
+ * you can add 1 or more orders
+ */
+async function createMultiDenom(data) {
+    const headers = {
+        'integrations-order': token
+    };
+
+    /** Example value data :
+     * $data_customer = [
+     *  [
+     *      "customerNo" => "081212345678",
+     *      "customerEmail" => "",
+     *      "customerName" => "Test User",
+     *      "productCode" => "OVO20H",
+     *  ],
+     *  [
+     *      "customerNo" => "081199991111",
+     *      "customerEmail" => "",
+     *      "customerName" => "Test User2",
+     *      "productCode" => "OVO10H",
+     *  ],         * 
+     * ]
+     */
+    const api = axios.create({ headers, responseType: 'json' });
+    const result = await api.post(`${main_url}order-integration/bulk/multi-denom`, data)
+        .then(resp => {
+            return resp.data;
+        });
+    return result;
+}
+
+async function redeemMethodFile(token, number) {
+    const headers = {
+        'integrations-order': token
+    };
+    const data =
+    {
+        "token": token,
+        "number": number
+    }
+    const api = axios.create({ headers, responseType: 'json' });
+    const result = await api.post(`${main_url}order-integration/redeem`, data)
+        .then(resp => {
+            return resp.data;
+        });
+    return result;
+}
+
---
+++ php sample/index.php
@@ -0,0 +1,238 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+
+    $GLOBALS['main_url'] = "https://beta.rewardplus-staging.com/";
+    $GLOBALS['token'] = "156a48c8-eb09-4d06-81ae-d730ab7c7207";
+
+    /** CURL GET */
+    function get_http_request($url)
+    {
+        $ch = curl_init();
+
+        /**set url  */
+        curl_setopt($ch, CURLOPT_URL, $GLOBALS['main_url'] . $url);
+
+        /** return the transfer as a string */
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLINFO_HEADER_OUT, true);
+        /** $output contains the output string */
+        curl_setopt(
+            $ch,
+            CURLOPT_HTTPHEADER,
+            array(
+                'Content-Type: application/json',
+                'integrations-order: ' . $GLOBALS['token'],
+            )
+        );
+        $output = curl_exec($ch);
+
+        /**close curl  */
+        curl_close($ch);
+
+        /**menampilkan hasil curl */
+        return $output;
+    }
+
+    /** CURL POST */
+    function post_http_request($url, $data)
+    {
+        $ch = curl_init();
+
+        $payload = json_encode($data);
+        /** set url */
+        curl_setopt($ch, CURLOPT_URL, $GLOBALS['main_url'] . $url);
+        /** return the transfer as a string */
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $payload);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+        /**Set HTTP Header for POST request */
+        curl_setopt(
+            $ch,
+            CURLOPT_HTTPHEADER,
+            array(
+                'Content-Type: application/json',
+                'integrations-order: ' . $GLOBALS['token'],
+            )
+        );
+        /** $output contains the output string */
+        $output = curl_exec($ch);
+
+        /** close curl */
+        curl_close($ch);
+
+        /** show result curl */
+        return $output;
+    }
+
+    /** Check your account info*/
+    function AccountInfo()
+    {
+        $response = get_http_request('order-integration/account');
+        echo $response;
+    }
+
+    /** check product list , you can filter by exired_date, category or name*/
+    function getProductList($expiry, $category, $name)
+    {
+        $url = "order-integration/products?page=1&itemsPerPage=10&sortBy=createdAt&descending=true&filter[expiry]=$expiry&filter[category]=$category&filter[name]=$name";
+        $response = get_http_request($url);
+        echo $response;
+    }
+
+    /** check voucher list you also can filter by name*/
+    function getVoucherList($name)
+    {
+        $url = "order-integration/vouchers?filter[name]=$name&itemsPerPage=10&sortBy=id&descending=true";
+        $response = get_http_request($url);
+        echo $response;
+    }
+
+    /** check list bank exist*/
+    function getBankList()
+    {
+        $url = "order-integration/bank-list";
+        $response = get_http_request($url);
+
+        echo $response;
+    }
+
+    /** check your order detail by id*/
+    function getOrderDetail($id_order)
+    {
+        $url = "order-integration/orders/$id_order";
+        $response = get_http_request($url);
+        echo $response;
+    }
+
+    /** check order list you can filter by bankcode, price, productCode or transId*/
+    function getOrderList($bankCode, $price, $productCode, $transId)
+    {
+        $url = "order-integration/orders?page=1&itemsPerPage=1&sortBy=createdAt&descending=false&filter[bankCode]=$bankCode&filter[price]=$price&filter[productCode]=$productCode&filter[transId]=$transId";
+        $response = get_http_request($url);
+        echo $response;
+    }
+
+
+    /**check order filter by BulkId (you can get bulk Id when create Order)*/
+    function getOrderbyBulkId($bulkId)
+    {
+        $url = "order-integration/orders?filter[bulkId]=$bulkId";
+        $response = get_http_request($url);
+        echo $response;
+    }
+
+    /** create evoucher link 
+     * Method FILE for generate Links
+     * example ProductType = BANK
+     */
+    function createEvoucherLink($productCode, $productType, $amount, $count)
+    {
+        $url = "order-integration/bulk-order";
+        /** add your data
+         * if productCode status isDenom == true && productType == EWALLET, you have to put amount. 
+         * Becouse that product doesn't have fixed price.
+         * if ProductType == BANK, you have to put amount.
+         */
+        $data = [
+            "productCode" => $productCode,
+            "productType" => $productType,
+            "amount" => $amount,
+            "deliveryMethod" => "FILE",
+            "useInventoryService" => "true",
+            "count" => $count
+        ];
+
+        $response = post_http_request($url, $data);
+        echo $response;
+    }
+
+    /** create order Bulk example: productType = EWALLET */
+    function createDirectSending($productCode, $productType, $amount, $count)
+    {
+        $url = "order-integration/bulk-order";
+        /** add your data
+         * if productCode status isDenom == true && productType == EWALLET, you have to put amount. 
+         * Becouse that product doesn't have fixed price.
+         * if ProductType == BANK, you have to put amount.
+         */
+        $data = [
+            "productCode" => $productCode,
+            "productType" => $productType,
+            // "amount" => $amount,
+            "deliveryMethod" => "BULK",
+            "useInventoryService" => "true",
+            "customers" => [
+                [
+                    "customerNo" => "0811999999991111",
+                    "customerEmail" => "",
+                    "customerName" => "Test User"
+                ]
+            ]
+        ];
+
+        $response = post_http_request($url, $data);
+        echo $response;
+    }
+
+    /** create order multi-denom
+     * you can add 1 or more orders
+     */
+    function createMultiDenom($data_customer, $productType, $deliveryMethod)
+    {
+        $url = "order-integration/bulk/multi-denom";
+
+        /** Example value $data_customer :
+         * $data_customer = [
+         *  [
+         *      "customerNo" => "081212345678",
+         *      "customerEmail" => "",
+         *      "customerName" => "Test User",
+         *      "productCode" => "OVO20H",
+         *  ],
+         *  [
+         *      "customerNo" => "081199991111",
+         *      "customerEmail" => "",
+         *      "customerName" => "Test User2",
+         *      "productCode" => "OVO10H",
+         *  ],         * 
+         * ]
+         */
+        $data = [
+            "productType" => $productType,
+            "deliveryMethod" => $deliveryMethod,
+            "useInventoryService" => "true",
+            "customers" => $data_customer
+        ];
+        $response = post_http_request($url, $data);
+        echo $response;
+    }
+    function redeemMethodFile($token, $number)
+    {
+        $url = "order-integration/redeem";
+
+
+        $data = [
+            "token" => $token,
+            "number" => $number,
+        ];
+        $response = post_http_request($url, $data);
+        echo $response;
+    }
+
+    ?>
+
+
+
+</body>
+
+</html>
